98f8a47448fcad1bf5dd428dd4ee0a404d9f0dc9
Nastya <anastasilerm@mail.ru>
2024-09-05 18:55:56 +0300

    fix test.sh

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@
 docker-compose up -d
 
 # Регистрация пользователя
-docker-compose exec client ./cli -register -username nastya -password nastya -url http://server:8080
+docker-compose exec client ./cli -register -login user -password test -url http://server:8080
 
 # Логин и получение токена
-TOKEN=$(docker-compose exec client ./cli -login -username testuser -password testpassword -url http://server:8080 | grep 'Token:' | awk '{print $2}')
+TOKEN=$(docker-compose exec client ./cli -auth -login user -password test -url http://server:8080 | grep 'Token:' | awk '{print $2}')
 
 # Проверяем, что токен был получен
 if [ -z "$TOKEN" ]; then
@@ -15,5 +15,18 @@ if [ -z "$TOKEN" ]; then
   exit 1
 fi
 
+# Копирование файла на контейнер
+docker cp /your/path/file.txt clientserverapp-client-1:/app/file.txt
+
 # Загрузка файла
 docker-compose exec client ./cli -upload /app/file.txt -token $TOKEN -url http://server:8080
+
+# Проверяем, что файл был загружен успешно
+
+# Скачивание файла
+docker-compose exec client ./cli -download file.txt -token $TOKEN -url http://server:8080
+
+# Проверяем, что файл был скачан успешно
+
+# Остановка и удаление контейнеров
+docker-compose down
